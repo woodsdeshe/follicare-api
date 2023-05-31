405944beba6bda9ed185e8b2491f6a34cc19d8b9
DeShe Woods <woodsdeshe94@gmail.com>
2023-05-31 16:25:24 -0400

    First commit

---
+++ src/main/java/com/example/Follicare/FollicareApplication.java
@@ -0,0 +1,13 @@
+package com.example.Follicare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FollicareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FollicareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Follicare/FollicareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Follicare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FollicareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
