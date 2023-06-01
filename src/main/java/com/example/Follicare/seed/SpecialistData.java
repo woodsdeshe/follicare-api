3a34c0973238874cf2a0b6b280cd79c642bcdc3c
DeShe Woods <woodsdeshe94@gmail.com>
2023-06-01 17:11:03 -0400

    Finished SpecialistData in SpecialistData

--- src/main/java/com/example/Follicare/seed/SpecialistData.java
+++ src/main/java/com/example/Follicare/seed/SpecialistData.java
@@ -25,5 +25,11 @@ public class SpecialistData {
     private void seedData() {
         // Seed data for initial setup
         Specialist specialist1 = new Specialist(1L, "Brandy", "Singer", "Alopecia Areata", "49505", "brandy@test.com", "333-333-3333");
+        Specialist specialist2 = new Specialist(2L, "Byron", "Test", "Alopecia Areata", "49502", "byron@test.com", "333-333-3333");
+        Specialist specialist3 = new Specialist(3L, "Darius", "Lewis", "Alopecia Areata", "48205", "darius@test.com", "333-333-3333");
+
+        specialists.add(specialist1);
+        specialists.add(specialist2);
+        specialists.add(specialist3);
     }
 }
