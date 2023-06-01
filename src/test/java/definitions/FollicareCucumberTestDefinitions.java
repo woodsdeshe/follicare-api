8a68e9713855f2a08b68fdc6f60e032538684435
DeShe Woods <woodsdeshe94@gmail.com>
2023-06-01 18:01:14 -0400

    Completed Given for  thereAreMultipleSpecialistsInTheSystem() scenario in FollicareCucumberTestDefinitions

--- src/test/java/definitions/FollicareCucumberTestDefinitions.java
+++ src/test/java/definitions/FollicareCucumberTestDefinitions.java
@@ -1,15 +1,40 @@
 package definitions;
 
 import com.example.Follicare.FollicareApplication;
+import com.example.Follicare.repository.SpecialistRepository;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.cucumber.spring.CucumberContextConfiguration;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @CucumberContextConfiguration
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = FollicareApplication.class)
 public class FollicareCucumberTestDefinitions {
+
+    private static final String BASE_URL = "http://localhost";
+    private static Response response;
+    private static ResponseEntity<String> responseEntity;
+    private static List<?> list;
+    private static RequestSpecification request;
+
+    @Autowired
+    private SpecialistRepository specialistRepository;
+
+    @LocalServerPort
+    String port;
+
+
     @Given("a user has a valid profile and a specialist exists")
     public void aUserHasAValidProfileAndASpecialistExists() {
     }
@@ -26,8 +51,8 @@ public class FollicareCucumberTestDefinitions {
 
     @Given("there are multiple specialists in the system")
     public void thereAreMultipleSpecialistsInTheSystem() {
-
-
+        responseEntity = new RestTemplate().exchange(BASE_URL + port + "/api/specialists", HttpMethod.GET, null, String.class);
+        list = JsonPath.from(String.valueOf(responseEntity.getBody())).get();
     }
 
     @When("the user requests the list of all specialists")
