1a1a1e0196c64ee486b577efcad99041ff795481
DeShe Woods <woodsdeshe94@gmail.com>
2023-06-01 13:24:57 -0400

    Autowired SpecialistRepository in SpecialistService

--- src/main/java/com/example/Follicare/service/SpecialistService.java
+++ src/main/java/com/example/Follicare/service/SpecialistService.java
@@ -1,9 +1,16 @@
 package com.example.Follicare.service;
 
+import com.example.Follicare.repository.SpecialistRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SpecialistService {
 
+    private SpecialistRepository specialistRepository;
 
+    @Autowired
+    public void setSpecialistRepository(SpecialistRepository specialistRepository) {
+        this.specialistRepository = specialistRepository;
+    }
 }
