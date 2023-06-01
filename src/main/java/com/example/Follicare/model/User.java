4c58288d39d841b4d4b5b339c048072233b5bb34
DeShe Woods <woodsdeshe94@gmail.com>
2023-05-31 21:41:44 -0400

    Completed User model in User.java

--- src/main/java/com/example/Follicare/model/User.java
+++ src/main/java/com/example/Follicare/model/User.java
@@ -1,4 +1,73 @@
 package com.example.Follicare.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class User {
+    @Column
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String userName;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column
+    private String password;
+
+    public User() {
+    }
+
+    public User(Long id, String userName, String email, String password) {
+        this.id = id;
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", userName='" + userName + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
