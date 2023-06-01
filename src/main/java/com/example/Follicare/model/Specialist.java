90e117a3fc0a0d278884c5f33addff71f65e3bfd
DeShe Woods <woodsdeshe94@gmail.com>
2023-06-01 16:33:30 -0400

    Started seed data in DataLoader and finished Specialist model

--- src/main/java/com/example/Follicare/model/Specialist.java
+++ src/main/java/com/example/Follicare/model/Specialist.java
@@ -37,4 +37,93 @@ public class Specialist {
     @OneToMany(mappedBy = "specialist",cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Favorites> favorites;
 
+    public Specialist() {
+    }
+
+    public Specialist(Long id, String firstName, String lastName, String specialty, Character zipCode, String email, String phoneNumber) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.specialty = specialty;
+        this.zipCode = zipCode;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getSpecialty() {
+        return specialty;
+    }
+
+    public void setSpecialty(String specialty) {
+        this.specialty = specialty;
+    }
+
+    public Character getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(Character zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public List<Favorites> getFavorites() {
+        return favorites;
+    }
+
+    public void setFavorites(List<Favorites> favorites) {
+        this.favorites = favorites;
+    }
+
+    @Override
+    public String toString() {
+        return "Specialist{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", specialty='" + specialty + '\'' +
+                ", zipCode=" + zipCode +
+                ", email='" + email + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/example/Follicare/seed/DataLoader.java
+++ src/main/java/com/example/Follicare/seed/DataLoader.java
@@ -1,4 +1,26 @@
 package com.example.Follicare.seed;
 
-public class DataLoader {
+import com.example.Follicare.model.Specialist;
+import com.example.Follicare.repository.SpecialistRepository;
+import com.example.Follicare.service.SpecialistService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements CommandLineRunner {
+
+    @Autowired
+    SpecialistRepository specialistRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        loadUserData();
+    }
+
+    private void loadUserData() {
+        if (specialistRepository.count() == 0) {
+            Specialist specialist = new Specialist(1L, "Billy", "Joe")
+        }
+    }
 }
