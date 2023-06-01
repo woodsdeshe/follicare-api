3210c7737f1c65fc8474f013ac736ba63f853861
DeShe Woods <woodsdeshe94@gmail.com>
2023-05-31 20:57:59 -0400

    Changed FollicareCucumberTestFeature.feature location and added aUserHasAValidProfileAndASpecialistExists() in FollicareCucumberTestDefinitions

---
+++ src/test/java/definitions/FollicareCucumberTestDefinitions.java
@@ -0,0 +1,10 @@
+package definitions;
+
+import io.cucumber.java.en.Given;
+
+public class FollicareCucumberTestDefinitions {
+    @Given("a user has a valid profile and a specialist exists")
+    public void aUserHasAValidProfileAndASpecialistExists() {
+
+    }
+}
