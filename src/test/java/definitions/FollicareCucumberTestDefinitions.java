1748fa9877f7921aefc58818a74407c96cb228b6
DeShe Woods <woodsdeshe94@gmail.com>
2023-05-31 21:03:39 -0400

    Added annotations to  FollicareCucumberTestDefinitions

--- src/test/java/definitions/FollicareCucumberTestDefinitions.java
+++ src/test/java/definitions/FollicareCucumberTestDefinitions.java
@@ -1,7 +1,12 @@
 package definitions;
 
+import com.example.Follicare.FollicareApplication;
 import io.cucumber.java.en.Given;
+import io.cucumber.spring.CucumberContextConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@CucumberContextConfiguration
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = FollicareApplication.class)
 public class FollicareCucumberTestDefinitions {
     @Given("a user has a valid profile and a specialist exists")
     public void aUserHasAValidProfileAndASpecialistExists() {
