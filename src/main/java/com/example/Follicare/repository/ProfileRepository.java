e7085668b75bbf3e82470e0f2e59faf573b9b400
DeShe Woods <woodsdeshe94@gmail.com>
2023-06-02 16:00:50 -0400

    Added findProfilesByUser_Id in ProfileRepository

--- src/main/java/com/example/Follicare/repository/ProfileRepository.java
+++ src/main/java/com/example/Follicare/repository/ProfileRepository.java
@@ -3,7 +3,9 @@ package com.example.Follicare.repository;
 import com.example.Follicare.model.Profiles;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProfileRepository extends JpaRepository<Profiles, Long> {
 
-
+    List<Profiles> findProfilesByUser_Id(Long userId);
 }
