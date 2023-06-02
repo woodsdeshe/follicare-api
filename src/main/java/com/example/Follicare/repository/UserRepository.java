fde149885e99224be49d83d5cd46810799b374a8
DeShe Woods <woodsdeshe94@gmail.com>
2023-06-02 10:43:10 -0400

    Created UserRepository and added existsByEmail and findUserByEmail

---
+++ src/main/java/com/example/Follicare/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.example.Follicare.repository;
+
+import com.example.Follicare.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    // to register
+    boolean existsByEmail(String email);
+
+    // to login
+    User findUserByEmail(String email);
+}
