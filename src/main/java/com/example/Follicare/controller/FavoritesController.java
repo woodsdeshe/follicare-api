1bb05aa2b3d49e4d230768497b12fbdb59dc56f3
DeShe Woods <woodsdeshe94@gmail.com>
2023-06-03 09:03:46 -0400

    Completed path for getFavoritesForProfile in FavoritesController

---
+++ src/main/java/com/example/Follicare/controller/FavoritesController.java
@@ -0,0 +1,26 @@
+package com.example.Follicare.controller;
+
+import com.example.Follicare.model.Specialist;
+import com.example.Follicare.service.FavoritesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/users/{profileId}/favorites")
+public class FavoritesController {
+
+    @Autowired
+    private FavoritesService favoritesService;
+
+    // Functionality: Returns a lists of specialists in a users favorites list
+    // Path:  http://localhost:8080/api/users/{profileId}/favorites
+
+    public List<Specialist> getFavoritesForProfile(@PathVariable Long profileId) {
+        return favoritesService.getFavoritesForProfile(profileId);
+    }
+
+}
