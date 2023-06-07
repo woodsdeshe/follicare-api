628422cf28ebd0f2a87083703c0287ae74ca4dcd
DeShe Woods <woodsdeshe94@gmail.com>
2023-06-07 12:48:39 -0400

    Working CorsConfig

--- src/main/java/com/example/Follicare/CorsConfig.java
+++ src/main/java/com/example/Follicare/CorsConfig.java
@@ -12,18 +12,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @Configuration
-public class CorsConfig {
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowedOrigins("*")
-                        .exposedHeaders("*");
-            }
-        };
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowedOriginPatterns("*")
+                .exposedHeaders("*")
+                .allowCredentials(true);
     }
 }
